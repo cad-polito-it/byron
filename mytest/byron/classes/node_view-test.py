0b5306598b0dbf06abb04b53f81dd3483b165aac
hooman miryavifard <h.miryavifard@gmail.com>
2023-12-15 11:34:24 +0100

    editted

--- mytest/byron/classes/node_view-test.py
+++ mytest/byron/classes/node_view-test.py
@@ -2,79 +2,129 @@ import pytest
 import networkx as nx
 from byron.classes.node_view import NodeView
 from byron.classes.node_reference import NodeReference
-from byron.classes.selement import SElement  # Replace with actual import
+from byron.classes.node import Node
+from byron.classes.selement import SElement
+from byron.classes.parameter import ParameterABC
+from byron.classes.value_bag import ValueBag
+from typing import Any
 
-# Assuming a basic implementation of SElement
+
 class MockSElement(SElement):
-    def __init__(self):
+    def dump(self, value_bag):
+        return "MockDump" 
+    
+class MockParameter(ParameterABC):
+    def __init__(self, value):
+        super().__init__()
+        self._value = value  
+
+    def mutate(self, strength: float = 1.0) -> None:
         pass
 
-    def dump(self, value_bag):
-        return "MockSElement dump"
+    def is_correct(self, obj: Any) -> bool:
+     
+        return True
+    
 
-# Setup a graph for testing
 @pytest.fixture
-def setup_graph():
+def mock_graph():
     G = nx.MultiDiGraph()
-    # Add nodes and edges to the graph as necessary for testing
-    for i in range(1, 4):
-        G.add_node(i, _type="SomeType", _selement=MockSElement())
-    G.add_edge(1, 2, _type="FRAMEWORK")
-    G.add_edge(2, 3, _type="FRAMEWORK")
+    G.add_node(0, _type='Type', _selement=MockSElement(), param=MockParameter(42))
+    G.add_node(1, _type='Type', _selement=MockSElement())
+    G.add_edge(0, 1, _type='FRAMEWORK')
     return G
 
-# Test NodeView creation and basic properties
-def test_node_view_creation(setup_graph):
-    node_ref = NodeReference(graph=setup_graph, node=1)
-    node_view = NodeView(ref=node_ref)
+@pytest.fixture
+def node_view_instance(mock_graph):
+    return NodeView(NodeReference(mock_graph, Node(0)))
 
-    assert node_view.ref == node_ref
-    assert node_view.graph is setup_graph
-    assert node_view.node == 1
-    assert isinstance(node_view.selement, MockSElement)
 
-# Test NodeView methods and properties
-def test_node_view_properties(setup_graph):
-    node_ref = NodeReference(graph=setup_graph, node=1)
-    node_view = NodeView(ref=node_ref)
+def test_node_view_initialization(mock_graph):
+    nv = NodeView(NodeReference(mock_graph, Node(0)))
+    assert isinstance(nv, NodeView)
+    assert nv.node == 0
 
-    # Test safe_dump property
-    assert node_view.safe_dump == "MockSElement dump"
 
-    # Test node_type property
-    assert node_view.node_type == "SomeType"
+def test_graph_property(node_view_instance):
+    assert isinstance(node_view_instance.graph, nx.MultiDiGraph)
 
-    # Test path_string property
-    assert node_view.path_string == "1.2"
 
-    # Test tree property
-    tree = node_view.tree
+def test_node_property(node_view_instance):
+    assert node_view_instance.node == 0
+
+
+def test_selement_property(node_view_instance):
+    assert isinstance(node_view_instance.selement, MockSElement)
+
+
+def test_type_property(node_view_instance):
+    assert node_view_instance.type_ == MockSElement
+
+
+def test_node_type_property(node_view_instance):
+    assert node_view_instance.node_type == 'Type'
+
+def test_path_string_property(node_view_instance):
+    assert isinstance(node_view_instance.path_string, str)
+
+def test_node_attributes_property(node_view_instance):
+    attributes = node_view_instance.node_attributes
+    assert isinstance(attributes, dict)
+    assert '_type' in attributes
+    assert '_selement' in attributes
+
+
+def test_p_property(node_view_instance):
+    p = node_view_instance.p
+    assert isinstance(p, dict)
+    assert 'param' in p
+    assert p['param'] == 42
+
+
+def test_tree_property(node_view_instance):
+    tree = node_view_instance.tree
     assert isinstance(tree, nx.DiGraph)
-    assert set(tree.nodes) == set(setup_graph.nodes)
 
-    # Test parent property
-    parent_view = node_view.parent
-    assert isinstance(parent_view, NodeView)
-    assert parent_view.node == 0  # Assuming 0 is the root node
 
-    # Test children property
-    children_views = node_view.children
-    assert all(isinstance(child, NodeView) for child in children_views)
-    assert [child.node for child in children_views] == [2]  # Node 1 has one child, Node 2
+def test_parent_property(node_view_instance):
+    parent = node_view_instance.parent
+    assert isinstance(parent, NodeView)
+    assert parent.node == 0
 
-    # Test path property
-    path = node_view.path
-    assert all(isinstance(pv, NodeView) for pv in path)
-    assert [pv.node for pv in path] == [0, 1]
 
-    # Test out_degree property
-    assert node_view.out_degree == 1  # Node 1 has one outgoing edge of type FRAMEWORK
+def test_children_property(node_view_instance):
+    children = node_view_instance.children
+    assert isinstance(children, list)
+    for child in children:
+        assert isinstance(child, NodeView)
 
-    # Add more tests for other properties and methods as needed
 
-# Test NodeView.make static method
-def test_node_view_make(setup_graph):
-    node_view = NodeView.make(setup_graph, 1)
-    assert isinstance(node_view, NodeView)
-    assert node_view.node == 1
+def test_path_property(node_view_instance):
+    path = node_view_instance.path
+    assert isinstance(path, tuple)
+    for node in path:
+        assert isinstance(node, NodeView)
 
+def test_out_degree_property(node_view_instance):
+    out_degree = node_view_instance.out_degree
+    assert isinstance(out_degree, int)
+
+def test_safe_dump(node_view_instance):
+    dumped = node_view_instance.safe_dump
+    assert isinstance(dumped, str)
+    assert dumped == "MockDump"
+
+def test_fields_property(node_view_instance):
+    fields = node_view_instance.fields
+    assert isinstance(fields, list)
+    assert 'graph' in fields
+    assert 'node' in fields
+
+def test_make_static_method(mock_graph):
+    nv = NodeView.make(mock_graph, 0)
+    assert isinstance(nv, NodeView)
+    assert nv.node == 0
+
+
+if __name__ == "__main__":
+    pytest.main()
