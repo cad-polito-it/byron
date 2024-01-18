c07ab92f643091320d3e4b4d827dbddf961ca702
hooman miryavifard <h.miryavifard@gmail.com>
2024-01-18 13:34:27 +0100

    edit for selement errors

--- byron/classes/selement.py
+++ byron/classes/selement.py
@@ -23,7 +23,7 @@
 
 # =[ HISTORY ]===============================================================
 # v1 / June 2023 / Squillero (GX)
-
+"""THE FIRST ONE IS FOR THE DEFINING CLASSES AND THE SECOND ONE IS FOR THE DEFINING METACLASS. """
 __all__ = ['SElement', 'SElementMeta']
 
 from typing import Callable, Sequence, Optional
@@ -164,7 +164,8 @@ class SElement(metaclass=SElementMeta):
         return id(self) == id(other)
 
     def __hash__(self):
-        return id()
+        return id(self)
+        #todo shouldnt this be self??
 
     @classmethod
     def add_node_check(cls, function: Callable) -> None:
